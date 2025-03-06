4988195af583194c3e86be0a3de1f4680c92e5e4
Madalina Oprina <malina.oprina@yahoo.com>
2025-03-06 19:45:58 +0200

    Add files via upload

---
+++ Arduino thermometer.ino
@@ -0,0 +1,38 @@
+#include "DHT.h"
+#define DHTPIN 14   
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+  pinMode(25,OUTPUT);
+}
+
+void loop() {
+  
+  delay(2000);
+
+  float h;
+  h = dht.readHumidity();
+  if(h>30 & h<60)
+    digitalWrite(25,HIGH);
+  else digitalWrite(25,LOW);
+  float t = dht.readTemperature();
+  
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  
+  Serial.print(F("Temperatură: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F("Umiditate: "));
+  Serial.print(h);
+  Serial.println(F("%"));
+
+  
+}
